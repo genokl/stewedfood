1c2707813195349f1bc44fda6058c8b92cc630bf
wangxinyu <wang.xinyu@epc-china.com>
2019-06-28 16:12:53 +0800

    1

---
+++ src/main/java/cn/zgyt/MyConfiguration.java
@@ -0,0 +1,24 @@
+package cn.zgyt;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import cn.zgyt.util.xcx.XcxConfig;
+
+@Configuration
+public class MyConfiguration extends WebMvcConfigurerAdapter {
+
+	@Autowired
+	private XcxConfig xcxConfig;
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        //和页面有关的静态目录都放在项目的static目录下
+//        registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
+        //上传的图片在D盘下的OTA目录下，访问路径如：http://localhost:8081/OTA/d3cf0281-bb7f-40e0-ab77-406db95ccf2c.jpg
+        //其中OTA表示访问的前缀。"file:D:/OTA/"是文件真实的存储路径
+        registry.addResourceHandler("/xcximg/**").addResourceLocations("file:E:/git/xcxpic/");
+        super.addResourceHandlers(registry);
+    }
+}
